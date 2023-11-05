191fc788d0056b4d8b14ddf165c3457b3ef2e507
Francis Acquah <cylic24darers@gmail.com>
2023-11-05 08:19:32 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,19 +15,21 @@ int main(int argc, char *argv[])
 	int coins[] = {25, 10, 5, 2, 1};
 	int tot_coins = sizeof(coins) / sizeof(coins[0]);
 
-	cents = atoi(argv[1]);
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
+	cents =atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
+
 	counter = 0;
+
 	for (i = 0; i < tot_coins; i++)
 	{
 		while (cents >= coins[i])
