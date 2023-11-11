acacff350c7ee0ddc28fd2401c7db59e1e5ecad0
Francis Acquah <cylic24darers@gmail.com>
2023-11-11 15:24:47 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,13 +12,13 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	int len_str1 = 0;
 	int len_str2 = 0;
-	char* tot_str ;
+	char *tot_str;
 
 	if (s1 == NULL)
-		s1 = (" ");
+		s1 = ("");
 
 	if (s2 == NULL)
-		s2 = (" ");
+		s2 = ("");
 
 
 	while (s1[len_str1] != '\0')
@@ -26,22 +26,22 @@ char *str_concat(char *s1, char *s2)
 		len_str1++;
 	}
 
-	while ( s2[len_str2] != '\0')
+	while (s2[len_str2] != '\0')
 	{
 		len_str2++;
 	}
 
-	tot_str = (char*)malloc((len_str1 + len_str2 + 1) *sizeof(char));
+	tot_str = (char *)malloc((len_str1 + len_str2 + 1) * sizeof(char));
 
 	if (tot_str == NULL)
 		return (NULL);
 
-	for (i = 0;i < len_str1; i++)
+	for (i = 0; i < len_str1; i++)
 	{
 		tot_str[i] = s1[i];
 	}
 
-	for (j = 0;j < len_str2; j++,i++)
+	for (j = 0; j < len_str2; j++, i++)
 	{
 		tot_str[i] = s2[j];
 	}
