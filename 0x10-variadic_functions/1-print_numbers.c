a24adea79f154ca5a201273365ff320fbc302dd5
Francis Acquah <cylic24darers@gmail.com>
2023-11-18 17:45:49 -0800

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,46 +1,41 @@
 #include "variadic_functions.h"
 /**
  * print_numbers - function that prints numbers
- * @seperator: character string
+ * @separator: character string
  * @n: total number of args
  * Return: void
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-        int str_len = 0;
-        unsigned int i;
-	int j;
-        char *str;
-        va_list num_args;
-        va_start(num_args, n);
+	 int str_len = 0;
+	unsigned int i;
+	int j = 0;
+	char *str;
+	va_list num_args;
 
+	va_start(num_args, n);
 	if (separator == NULL)
 		exit(0);
-        while (separator[str_len] != '\0')
-                str_len++;
+	while (separator[str_len] != '\0')
+		str_len++;
 	str = (char *)malloc((sizeof(char) * str_len) + 1);
 	if (str == NULL)
-		exit(0);
-
-	j = 0;
-
-	while (separator[j] !=  '\0')
+		exit(0)
+	while (separator[j] != '\0')
 	{
 		str[j] = separator[j];
 		j++;
 	}
-
-        for (i = 0;i < n; i++)
-        {
-                printf("%d", va_arg(num_args,int));
-                if (i < n - 1)
-                {
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(num_args, int));
+		if (i < n - 1)
+		{
 			printf("%s", str);
-                 
-                }
-        }
+		}
+	}
 	free(str);
-        va_end(num_args);
-        printf("\n");
+	va_end(num_args);
+	printf("\n");
 }
