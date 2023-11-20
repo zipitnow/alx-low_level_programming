f6c40be35875a34aaedc84496361c81b39faedf4
Francis Acquah <cylic24darers@gmail.com>
2023-11-20 12:45:24 -0800

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *s;
+	va_list num_args;
+
+	va_start(num_args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(num_args, char *);
+		if (s == NULL)
+			printf("nil");
+		else
+			printf("%s",s);
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s",separator);
+
+	}
+
+	va_end(num_args);
+	printf("\n");
+}
