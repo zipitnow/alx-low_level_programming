da492854839f6d403c0e24be2d19d19171025993
Francis Acquah <cylic24darers@gmail.com>
2023-11-20 11:18:27 -0800

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,35 +8,16 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	 int str_len = 0;
 	unsigned int i;
-	int j = 0;
-	char *str;
 	va_list num_args;
 
 	va_start(num_args, n);
-	while (separator[str_len] != '\0')
-		str_len++;
-	str = (char *)malloc((sizeof(char) * str_len) + 1);
-	if (str == NULL)
-		exit(0);
-	while (separator[j] != '\0')
-	{
-		str[j] = separator[j];
-		j++;
-	}
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(num_args, int));
-		if (i < n - 1)
-		{
-			if (separator == NULL)
-				exit(0);
-			else
-				printf("%s", str);
-		}
+		if (i != (n - 1) && separator != NULL)
+				printf("%s", separator);
 	}
-	free(str);
 	va_end(num_args);
 	printf("\n");
 }
