36cde5584f0879fd124327b2f09a3565dd299311
Francis Acquah <cylic24darers@gmail.com>
2023-11-21 10:33:18 -0800

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,35 +9,42 @@
 
 void print_all(const char * const format, ...)
 {
-	int i = 0;
+	int i, bool_checker;
 	char *str;
 	va_list num_args;
 
 	va_start(num_args, format);
-	while (format && format[i])
+	i = 0;
+	while (format != NULL && format[i] != '\0')
 	{
 	switch (format[i])
 	{
 	case 'c':
 		printf("%c", va_arg(num_args, int));
+		bool_checker = 1;
 		break;
 	case 'i':
 		printf("%d", va_arg(num_args, int));
+		bool_checker = 1;
 		break;
 	case 'f':
 		printf("%f", va_arg(num_args, double));
+		bool_checker = 1;
 		break;
 	case 's':
 		str = va_arg(num_args, char *);
 		if (str == NULL)
-			printf("(nil)");
-		else
-			printf("%s", str);
+			str = "(nil)";
+		printf("%s", str);
+		bool_checker = 1;
 		break;
+	default:
+		bool_checker = 0;
 	}
-	i++;
-	if (format[i])
+
+	if (format[i + 1] != '\0' && bool_checker == 1)
 		printf(", ");
+	i++;
 	}
 	va_end(num_args);
 	printf("\n");
