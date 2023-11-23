ce3c3be98fdff13489cb74c349bcc33012ba0c83
Francis Acquah <cylic24darers@gmail.com>
2023-11-23 05:10:00 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
+/**
+ *struct dog - creating a stuct dog in header file
+ * @name: as string variable
+ * @age: as float variable
+ * @owner: as string variable
+ */
+
+struct dog
+{
 	char	*name;
 	float	age;
 	char	*owner;
