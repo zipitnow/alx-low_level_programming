e97cd94dc180cd2df65b73269dd243c9272130b0
Francis Acquah <cylic24darers@gmail.com>
2023-11-23 09:38:44 -0800

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, j, name_len, own_len;
+	dog_t *dog;
+
+	name_len = 0;
+	own_len = 0;
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return NULL;
+	if (name[name_len] != '\0')
+		name_len++;
+	if (owner[own_len] != '\0')
+		own_len++;
+	dog->name = malloc((sizeof(char) * name_len) + 1);
+        if (dog->name == NULL)
+        {
+		free(dog->name);
+                return NULL;
+        }
+	
+	if (dog->name == NULL)
+	{
+		return NULL;
+	}
+	dog->owner = malloc((sizeof(char) * own_len) + 1);
+	if (dog->owner == NULL)
+		return NULL;
+	 for (i = 0; i < name_len; i++)
+		 dog->name[i] = name[i];
+	  dog->name[i] = '\0';
+	for (j = 0; j < own_len; j++)
+		dog->owner[j] = owner[j];
+	dog->owner[j] = '\0';
+	dog->name = name;
+	dog->age = age;
+
+	return (dog);
+
+}
