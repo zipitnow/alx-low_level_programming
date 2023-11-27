9e7a3103946dd5b42227c3fee874a1546a4f03cb
Francis Acquah <cylic24darers@gmail.com>
2023-11-27 09:18:06 -0800

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all the
+ * elements of a list_t list
+ * @h: a pointer to a node
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h != NULL)
+	{
+		count++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n",h->len,h->str);
+		h = h->next;
+	}
+	return (count);
+}
