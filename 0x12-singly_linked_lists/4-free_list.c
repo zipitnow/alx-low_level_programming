b27e2b4d6ff14550d177fa6d5af1c48e9c370e89
Francis Acquah <cylic24darers@gmail.com>
2023-11-28 06:09:12 -0800

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,5 +2,14 @@
 
 void free_list(list_t *head)
 {
-
+	list_t *node_free = head;
+	list_t *next_free;
+	
+	while (node_free != NULL)
+	{
+		next_free = node_free->next;
+		free(node_free->str);
+		free(node_free);
+		node_free = next_free;
+	}
 }
