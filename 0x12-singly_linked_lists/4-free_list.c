55d6d597bb5bf87a0addb1a3e72ea99928fb5c89
Francis Acquah <cylic24darers@gmail.com>
2023-11-28 06:12:42 -0800

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,10 +1,15 @@
 #include "lists.h"
 
+/**
+ * free_list - function that frees a list_t list
+ * @head: pointer to head node
+ */
+
 void free_list(list_t *head)
 {
 	list_t *node_free = head;
 	list_t *next_free;
-	
+
 	while (node_free != NULL)
 	{
 		next_free = node_free->next;
