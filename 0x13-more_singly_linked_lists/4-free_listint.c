cda1f1369213d2786c81a390f32b184e1cd35964
Francis Acquah <cylic24darers@gmail.com>
2023-11-29 01:06:27 -0800

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a
+ * listint_t list
+ * @head: pointer to header node
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *next_node;
+
+	while (current->next != NULL)
+	{
+		next_node = current->next;
+		free(current);
+		current = next_node;
+	}
+}
