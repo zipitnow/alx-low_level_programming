6001d22d7f57fcae9723b9501220290735c99b98
Francis Acquah <cylic24darers@gmail.com>
2023-11-29 12:49:00 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,29 +14,30 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count = 0;
 	listint_t *current_node = malloc(sizeof(listint_t));
 	listint_t *next_pointer = *head;
-	listint_t *temp;
 
 	if (current_node == NULL)
 		return (NULL);
 	current_node->n = n;
 	if (idx == 0)
 	{
-		current_node = next_pointer->next;
+		current_node->next = *head;
 		*head = current_node;
 		return (current_node);
 	}
-	while (next_pointer != NULL)
+	else
 	{
-		if (count == idx - 1)
+		while (next_pointer != NULL)
 		{
-			temp = current_node->next;
-			current_node->next = next_pointer;
-			next_pointer->next = temp;
-			return (current_node);
+			if (count == idx-1)
+			{
+				current_node->next = next_pointer->next;
+				next_pointer->next = current_node;
+				return (current_node);
 
+			}
+			count++;
+			next_pointer = next_pointer->next;
 		}
-		count++;
-		next_pointer = next_pointer->next;
 	}
 	free(current_node);
 	return (NULL);
