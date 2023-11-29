7cd7ed41e1c9b2589c9d6de650ecce049249b26e
Francis Acquah <cylic24darers@gmail.com>
2023-11-29 08:23:53 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n){
 		return NULL;
 	current_node->n = n;
 
-	if (idx == 0)
+	while (idx == 0)
 	{
 		current_node = next_pointer->next;
 		*head = current_node->next;
