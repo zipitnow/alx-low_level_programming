fb6b127ef33c2ea721eb22e3f180226c25c20547
Francis Acquah <cylic24darers@gmail.com>
2023-11-29 13:49:41 -0800

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,6 +15,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current = *head;
 	listint_t *next;
 
+	if (current == NULL)
+		return (-1);
 	if (index == 0)
 	{
 		*head = current->next;
